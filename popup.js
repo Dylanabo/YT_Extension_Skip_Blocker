61d8ea303e8e5f1ac848bd10ea6ec7c86255f357
Dylanabo <dylan.abodi@epitech.eu>
2023-11-22 17:31:50 +0400

    Add new tab functionality to popup.html and popup.js

--- popup.js
+++ popup.js
@@ -1,10 +1,18 @@
 let isActive = false;
+let newTab = false;
 
 document.addEventListener('DOMContentLoaded', function () {
   chrome.storage.local.get(['isActive'], function (result) {
     isActive = result.isActive || false;
     document.getElementById('status').textContent = isActive ? "Active" : "Inactive";
     document.getElementById('active').checked = isActive;
+  });
+
+  chrome.storage.local.get(['newTab'], function (result) {
+    newTab = result.newTab || false;
+    document.getElementById('tabs-status').textContent = newTab ? "New tab" : "Same tab";
+    document.getElementById('tabs-status').checked = newTab;
+
   });
 });
 
@@ -15,6 +23,14 @@ if (document.getElementById("active")) {
     document.getElementById("status").textContent = isActive ? "Active" : "Inactive";
     chrome.storage.local.set({ 'isActive': isActive });
 
+  });
+}
+
+if (document.getElementById("newtabs")) {
+  document.getElementById("newtabs").addEventListener("click", function () {
+    newTab = !newTab;
+    document.getElementById("tabs-status").textContent = newTab ? "New tab" : "Same tab";
+    chrome.storage.local.set({ 'newTab': newTab });
   });
 }
 
@@ -41,8 +57,17 @@ function checkYoutubeAndAct(tab) {
       const videoId = tab.url.split("v=")[1].split("&")[0];
       // Récupérer l'URL de la vidéo
       const videoUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-      // Remplacer l'URL de la page par celle de la vidéo
-      chrome.tabs.update(tab.id, { url: videoUrl });
+      if (newTab) {
+        console.log("New tab");
+        // Créer un nouvel onglet
+        chrome.tabs.create({ url: videoUrl });
+        // Et retourner sur l'onglet précédent
+        chrome.tabs.update(tab.id, { url: "https://www.youtube.com/" });
+      } else {
+        console.log("Same tab");
+        // Remplacer l'URL de la page par celle de la vidéo
+        chrome.tabs.update(tab.id, { url: videoUrl });
+      }
     }
   }
 }
